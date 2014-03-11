ee4db29552d49ac6c93d206f999daa519a768400
Brecht Hoflack <brh@melexis.com>
2014-03-11 09:38:49 +0100

    CMGT-2 Finish parsing the cmdb page.

--- src/main/clj/cmis/cmdb.clj
+++ src/main/clj/cmis/cmdb.clj
@@ -8,22 +8,34 @@
             (if (not (nil? id))
               (.contains id expr))))))
 
-(defn html-id-like
-  [resource id]
+(defn- extract
+  [resource selectors]
   (->>
    (-> resource
-       (select [:body :div#content-core (id-contains? id) :span :> text-node])
+       (select selectors)
        rest)
    (clojure.string/join "\n")
    (clojure.string/trim)))
+
+(defn html-id-like
+  [resource id]
+  (extract resource [:body :div#content-core (id-contains? id) :span :> text-node]))
+
+(defn extract-critical-level
+  [resource id]
+  (extract resource [:body :div#content-core (id-contains? id) :> text-node]))
 
 (defn parse-product-page
   "Extract the product data from the product page."
   [^java.io.InputStream is]
   (let [res (-> is html-resource)]
-    {:background  (html-id-like res "Background")
+    {:name (clojure.string/trim
+            (-> res
+                (select [:body :h1#parent-fieldname-title :> text-node])
+                first))
+     :background  (clojure.string/replace (html-id-like res "Background") "\n" "")
      :functionality (html-id-like res "Functionality")
-                                        ;     :critical_level (Integer/parseInt (html-id-like res "CriticalLevel"))
+     :critical_level (Integer/parseInt (extract-critical-level res "CriticalLevel"))
      :business_owner (html-id-like res "BusinessOwner")
      :development (html-id-like res "Development")
      :ops_support (html-id-like res "OpsSupport")
